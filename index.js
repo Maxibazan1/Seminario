07cec7426ec8f69f9f03db6f05f18a246b4aad40
Maxi <fmbazan@institutosanmartin.com>
2024-08-25 18:30:23 -0300

    Frontend casi terminado

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000; // O cualquier otro puerto que prefieras
+
+// Configura el directorio 'Frontend' para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'Frontend')));
+
+// Ruta principal (puede ser la ruta de tu index.html)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Frontend', 'index.html'));
+});
+
+// Inicia el servidor
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
+
+
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
@@ -16,8 +36,6 @@ connection.connect((err) => {
 });
 
 module.exports = connection;
-
-
 
 
 
